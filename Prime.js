da2f173f41c924b8d909caadc373a5676f79dd16
sarosarventhiran <sarosarventhiran@gmail.com>
2026-07-02 12:05:43 +0530

    Update Prime.js

--- Prime.js
+++ Prime.js
@@ -4,8 +4,23 @@
  * @param {number} n - The number to check.
  * @returns {boolean} True if the number is prime, false otherwise.
  */
+//const n=parseInt();
+const n = parseInt(process.argv[2]);
 function isPrime(n) {
-    // type code here
+    if  (num <= 1) rturn false;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+if (isPrime(n)) {
+    console.log("Prime Number");
+} else {
+    console.log("?Not Prime Numbers");
+}
+module.exports = isPrime;
 
 module.exports = isPrime;
